8511d9e2a3b6678bc422e1dd8b0b4e3c92b4d812
b10t <b10t.rus@gmail.com>
2022-02-21 11:48:40 +0800

    Deleting unnecessary fields in Flat.

--- property/admin.py
+++ property/admin.py
@@ -15,13 +15,11 @@ class FlatAdmin(admin.ModelAdmin):
                     'new_building',
                     'construction_year',
                     'town',
-                    'owners_phonenumber',
-                    'owner_pure_phone',
                     )
     list_editable = ['new_building']
     list_filter = ['new_building']
     readonly_fields = ['created_at']
-    search_fields = ('town', 'address', 'owner')
+    search_fields = ('town', 'address', )
     raw_id_fields = ['liked_by']
     inlines = [FlatsInstanceInline]
 
--- property/models.py
+++ property/models.py
@@ -5,10 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField(blank=True,
-                                        verbose_name='Нормализованный номер владельца')
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.NullBooleanField()
     created_at = models.DateTimeField(
         'Когда создано объявление',
